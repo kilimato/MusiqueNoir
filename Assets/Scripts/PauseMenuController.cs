9f451811d94c134e1b71bd5da90e67323f3412ab
tajomyll <tajomyll@student.jyu.fi>
2020-03-05 16:17:49 +0200

    Pause menu UI

---
+++ Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuController : MonoBehaviour
+{
+
+    private Canvas canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas = GetComponent<Canvas>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) canvas.enabled = !canvas.enabled;
+    }
+
+    public void ResumeGame()
+    {
+        canvas.enabled = false;
+    }
+}
