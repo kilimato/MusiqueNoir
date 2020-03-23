a88e8320cc02486d27bda06de318c542b37a02c3
kilimato <eeva.m.tolonen@student.jyu.fi>
2020-03-23 22:54:41 +0200

    Added platformerController to control stair colliders.

---
+++ Assets/Scripts/PlatformController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour
+{
+    private EdgeCollider2D edge;
+    private bool isTriggered = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        edge = GetComponent<EdgeCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTriggered)
+        {
+            edge.isTrigger = true;
+        }
+        if (!isTriggered)
+        {
+            edge.isTrigger = false;
+        }
+    }
+
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && Input.GetKey(KeyCode.S))
+        {
+            isTriggered = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isTriggered = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PlatformerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlatformerController : MonoBehaviour
+{
+    private EdgeCollider2D edge;
+    // Start is called before the first frame update
+    void Start()
+    {
+        edge = GetComponent<EdgeCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && Input.GetKey(KeyCode.S))
+        {
+            edge.isTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            edge.isTrigger = false;
+        }
+    }
+}
