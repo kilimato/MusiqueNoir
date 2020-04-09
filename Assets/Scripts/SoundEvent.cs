cd764eda81e98c99315610d1f53b0ad964c9f12f
jeonalli <55280110+jeonalli@users.noreply.github.com>
2020-04-09 11:10:26 +0300

    Footsteps
    Implemented footsteps to player and enemy animations and added FMOD files to .gitignore.

---
+++ Assets/Scripts/SoundEvent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundEvent : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string Event = "";
+
+    public void PlaySound()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(Event, transform.position);
+    }
+}
