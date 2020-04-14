5e0783eebb92a2775e75d580e921a37aec3e4c41
tajomyll <tajomyll@student.jyu.fi>
2020-04-14 09:31:09 +0300

    Added an intro screen with text explaining the premise.

--- Assets/Scripts/UIScripts/IntroTextController.cs
+++ Assets/Scripts/UIScripts/IntroTextController.cs
@@ -1,29 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class IntroTextController : MonoBehaviour
 {
 
     Canvas introCanvas;
+    RawImage fadeImage;
+    Text introText;
 
     // Start is called before the first frame update
     void Start()
     {
 
         introCanvas = GetComponent<Canvas>();
-        Time.timeScale = 0;
+        fadeImage = GetComponentInChildren<RawImage>();
+        introText = GetComponentInChildren<Text>();
+        //Time.timeScale = 0;
+        StartCoroutine(FadeIn(introText));
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (Input.anyKey && introCanvas.enabled == true)
+        if (Input.anyKey && introText.color.a >= 0.9f && introCanvas.enabled == true)
         {
-            introCanvas.enabled = false;
+            StartCoroutine(FadeOut(fadeImage));
+            StartCoroutine(FadeOut(introText));
+            //introCanvas.enabled = false;
             Time.timeScale = 1;
         }
-        
+
     }
+
+    IEnumerator FadeOut(Graphic g)
+    {
+
+        Color tempColor = g.color;
+
+        while (tempColor.a > 0)
+        {
+            tempColor.a -= 0.01f;
+            g.color = tempColor;
+            yield return null;
+
+        }
+
+    }
+
+    IEnumerator FadeIn(Graphic g)
+    {
+
+        Color tempColor = g.color;
+
+        while (tempColor.a < 1)
+        {
+            tempColor.a += 0.01f;
+            g.color = tempColor;
+            yield return null;
+
+        }
+
+    }
+
+
 }
