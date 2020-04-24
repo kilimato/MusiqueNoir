55842f7d2cf454fad8c431ff280f0909f1f24967
kilimato <eeva.m.tolonen@student.jyu.fi>
2020-04-24 14:42:04 +0300

    Added working start menu and settings menu, still in progress.

---
+++ Assets/PlatformController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour
+{
+    private EdgeCollider2D edgeCollider;
+    public bool isColliding = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        edgeCollider = GetComponent<EdgeCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.S) && isColliding )
+        {
+            Debug.Log("Collided with player");
+            edgeCollider.isTrigger = true;
+        }
+        if(!isColliding)
+        {
+            edgeCollider.isTrigger = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       isColliding = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isColliding = false;
+    }
+}
