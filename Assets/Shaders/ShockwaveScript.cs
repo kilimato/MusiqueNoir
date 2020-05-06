035dd3c0d2e8d7598b762bb9cdba948e33a97765
kilimato <eeva.m.tolonen@student.jyu.fi>
2020-05-06 19:48:10 +0300

    Testing wave effect, added placeholder textures and shaders for that.

---
+++ Assets/Shaders/ShockwaveScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShockwaveScript : MonoBehaviour
+{
+
+    private Vector3 myScale;
+    private float myCameraSize;
+
+
+    public float maxScale = 2f;
+    public Camera connectedCamera;
+    public float scaleMultiplier = 3f;
+    public float cameraSizeMultiplier = 0.5f;
+
+    void Start()
+    {
+        myScale = transform.localScale;
+        myCameraSize = connectedCamera.orthographicSize;
+    }
+
+
+    void Update()
+    {
+        transform.localScale += new Vector3(0.1f, 0.1f, 0.1f) * Time.deltaTime * scaleMultiplier;
+
+        connectedCamera.orthographicSize += Time.deltaTime * cameraSizeMultiplier;
+        //connectedCamera.rect = new Rect(m_ViewPositionX, m_ViewPositionY, m_ViewWidth, m_ViewHeight);
+
+        if (transform.localScale.x > maxScale)
+        {
+            transform.localScale = myScale;
+            connectedCamera.orthographicSize = myCameraSize;
+        }
+
+    }
+}
